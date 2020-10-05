5f28cdd78bee5c45c4083ecd2ad8281ecae1eb5f
Adriane Inocencio <adriane.0523@gmail.com>
2020-10-04 17:43:28 -0700

    forgot some items

--- server/routes/router.js
+++ server/routes/router.js
@@ -6,9 +6,6 @@ const Ctrl = require('../controllers/controller')
 const router = express.Router()
 
 router.post('/item', Ctrl.createItem)
-router.put('/item/:id', Ctrl.updateItem)
-router.delete('/item/:id', Ctrl.deleteItem)
-router.get('/item/:id', Ctrl.getItemById)
 router.get('/item', Ctrl.getItems)
 
 module.exports = router
